57ed43d33b5c636aa233a811e70a9992e72699bc
rudysc <42895863+rudysc@users.noreply.github.com>
2018-09-01 22:36:58 +0300

    Update segment.js

--- segment.js
+++ segment.js
@@ -1,3 +1,34 @@
+function getParams(script_name) {
+  // Find all script tags
+
+  var scripts = document.getElementsByTagName("script");
+  
+  // Look through them trying to find ourselves
+
+  for(var i=0; i<scripts.length; i++) {
+    if(scripts[i].src.indexOf("/" + script_name) > -1) {
+      // Get an array of key=value strings of params
+
+      var pa = scripts[i].src.split("?").pop().split("&");
+
+      // Split each key=value into array, the construct js object
+
+      var p = {};
+      for(var j=0; j<pa.length; j++) {
+        var kv = pa[j].split("=");
+        p[kv[0]] = kv[1];
+      }
+      return p;
+    }
+  }
+  
+  // No scripts match
+
+  return {};
+};
+
+var report_name = getParams("segment.js");
+
 ! function() {
     var analytics = window.analytics = window.analytics || [];
     if (!analytics.initialize)
@@ -29,6 +60,6 @@
             analytics.SNIPPET_VERSION = "4.1.0";
             analytics.load("3aFkqrZJpUaTAM9RBUL8RdHbTWTmNLGq");
             //analytics.page();
-            analytics.track('open_report', { report: 'TestReport', action: 'open' });
+            analytics.track('open_report', { report: report_name , action: 'open' });
         }
 }();
